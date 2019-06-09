1abe2bd41742ae036aadda8e3ca49ac0ad6ef5a8
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-06-09 22:57:37 +0200

    mapping to Movie class added

--- src/main/java/com/movies/client/MovieClient.java
+++ src/main/java/com/movies/client/MovieClient.java
@@ -44,5 +44,4 @@ public class MovieClient {
         }
         return null;
     }
-
 }
---
+++ src/main/java/com/movies/mapper/MovieMapper.java
@@ -0,0 +1,14 @@
+package com.movies.mapper;
+
+import com.movies.domain.Movie;
+import com.movies.domain.MovieDto;
+
+public class MovieMapper {
+    public Movie mapToMovie(MovieDto movieDto) {
+        return new Movie(
+                movieDto.getTitle(),
+                movieDto.getRelease_date(),
+                movieDto.getVote_average()
+        );
+    }
+}
